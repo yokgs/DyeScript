2261856c1dcb8edf8bb42b5484eeb9bfde088e2f
Yazid Slila <yslila@sqli.com>
2024-03-22 22:39:05 +0000

    clean parser implementation

---
+++ src/v1/Parser.ts
@@ -0,0 +1,55 @@
+export class Parser {
+
+    static readonly UP_ANCHOR = "^";
+    static readonly LEFT_ANCHOR = "<";
+    static readonly TEMPLATE_ANCHOR = /\{\*\}/g;
+    static readonly UP_TEMPLATE_ANCHOR = /\{\^\}/g;
+    static readonly LEFT_TEMPLATE_ANCHOR = /\{\<\}/g;
+
+
+    private replaceAnchors(commands: string[][]): string[][] {
+        let astrik = "";
+        let warn = true;
+
+        return commands.map((command, i) => {
+            return command.map((el, j) => {
+                if (el == Parser.UP_ANCHOR && i > 0) {
+                    astrik = commands[i - 1][j];
+                    warn = false;
+                    return astrik;
+                }
+
+                if (el == Parser.LEFT_ANCHOR && j > 0) {
+                    astrik = command[j - 1];
+                    warn = false;
+                    return astrik;
+                }
+
+                if (Parser.UP_TEMPLATE_ANCHOR.test(el) && i > 0) {
+                    astrik = commands[i - 1][j];
+                    warn = false;
+                    return el.replace(Parser.UP_TEMPLATE_ANCHOR, commands[i - 1][j]);
+                }
+
+                if (Parser.LEFT_TEMPLATE_ANCHOR.test(el) && j > 0) {
+                    astrik = command[j - 1];
+                    warn = false;
+                    return el.replace(Parser.LEFT_TEMPLATE_ANCHOR, command[j - 1]);
+                }
+
+                if (Parser.TEMPLATE_ANCHOR.test(el)) {
+                    if (warn) console.warn(`[WARN] Template "{*}" is used before being initilized`);
+                    return el.replace(Parser.TEMPLATE_ANCHOR, astrik);
+                }
+
+                return el;
+            });
+        });
+    }
+
+    public parse(source: string): string[][] {
+        let table = source.split(/[\r*\n]+/).map(x => x.split(/\s+/));
+        return this.replaceAnchors(table);
+    }
+
+}
