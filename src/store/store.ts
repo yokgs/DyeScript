7c53431dc5b1990a0c161923243a2253edd7ac13
yokgs <yazid.slila01@gmail.com>
2024-03-22 22:45:08 +0000

    redesign runtime data objects

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,8 +3,13 @@ import { IFont } from "../common/font.interface";
 import { IStyle } from "../common/style.interface";
 
 export class Store {
-    public styles: Map<string, IStyle>;
-    public fonts: Map<string, IFont>;
-    public animations: Map<string, IAnimation>;
-    public motions: Map<string, IAnimation>;
+    public styles: Map<string, IStyle> = new Map<string, IStyle>;
+    public fonts: Map<string, IFont> = new Map<string, IFont>();
+    public animations: Map<string, IAnimation> = new Map<string, IAnimation>();
+    public motions: Map<string, IAnimation> = new Map<string, IAnimation>();
+    public cachable: boolean = true;
+
+    extendFrom(store: Store): void {
+
+    }
 }
