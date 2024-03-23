f192a161ebed944dc271ddae09dbb3a36d65c1c5
Yazid Slila <yslila@sqli.com>
2024-03-23 01:30:16 +0000

    add source property

--- src/core/DyeRuntime.ts
+++ src/core/DyeRuntime.ts
@@ -1,21 +1,36 @@
 import { Store } from "../store/store";
+import { FileLoader } from "../fetch/FileLoader";
+import { Parser } from "../parser/Parser";
 
 export class DyeRuntime {
     static readonly version = 'pre-1.0.0';
     private store: Store;
+    private createdBy: string = 'api';
+    private source: string;
 
-    constructor() {
+    constructor(source: string) {
         this.store = new Store();
+        this.source = source;
     }
 
-    static createFromFile(filename: string): DyeRuntime {
-        return new DyeRuntime();
+    static createFromFile(filePath: string): DyeRuntime {
+        const content = FileLoader.getInstance().load(filePath);
+        return new DyeRuntime(content);
     }
 
     static createFromContent(content: string): DyeRuntime {
-        return new DyeRuntime();
+        return new DyeRuntime(content);
     }
 
-    
+    public restartFromSource(): DyeRuntime {
+        this.store = new Store();
+        this.initilize();
+        return this;
+    }
+
+    private initilize() {
+        let table = new Parser().parse(this.source);
+
+    }
 
 }
