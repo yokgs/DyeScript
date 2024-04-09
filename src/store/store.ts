6eb022f6f1a55862f6696cbb7883d3f3b1ede476
yokgs <yazid.slila01@gmail.com>
2024-04-09 02:10:39 +0000

    + Store extension

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,8 +8,27 @@ export class Store {
     public animations: Map<string, IAnimation> = new Map<string, IAnimation>();
     public motions: Map<string, IAnimation> = new Map<string, IAnimation>();
     public cachable: boolean = true;
+    private counter = 0;
 
     extendFrom(store: Store): void {
+        for (let key of store.styles.keys()) {
+            if (!this.styles.has(key)) this.styles.set(key, {} as IStyle);
+            this.mergeStyles(this.styles.get(key) || {}, store.styles.get(key) || {});
+        }
+    }
 
+    private mergeStyles(to: IStyle, from: IStyle): void {
+        for (let key in from) {
+            let fromProperty = from[key];
+            let calibratedProperty = fromProperty.map(([value, score]) => [value, score + this.counter]) as [string, number][];
+            if (key in to)
+                to[key].push(...calibratedProperty);
+            else
+                to[key] = calibratedProperty;
+        }
+    }
+
+    public tick() {
+        this.counter++;
     }
 }
