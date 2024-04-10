b0301608b48527789f67162f42159baf70b09442
yokgs <yazid.slila01@gmail.com>
2024-04-10 13:49:42 +0000

    + api

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,20 @@
-#!/usr/bin/env node
+import { DyeRuntime } from "./core/DyeRuntime";
 
-import yargs from 'yargs';
-import { DyeCompilerTarget } from './common/Target';
+class DyeScript {
+    private static instance: DyeScript;
+    private constructor() { }
 
-const { watcher } = require('./lib/Fetch/watch');
-const { load } = require('./lib/init');
+    public run(source: string): DyeRuntime {
+        return DyeRuntime.createFromContent(source);
+    }
 
-const options = yargs
-    .usage('Usage: dyec -p <path> -o <path> -t <target> -w')
-    .options({
-        p: { alias: 'path', describe: 'The path of the file', demandOption: true, type: 'string' },
-        o: { alias: 'output', describe: 'The path of the output directory', type: 'string' },
-        t: {
-            alias: 'target',
-            describe: 'The target style',
-            choices: ['r', 'a', 'c', 'm', 's'],
-            description: 'r: React StyleSheet; c: CSS; m: minified CSS; s: Static DyeScript; a: All',
-            type: 'string'
-        },
-        w: { alias: 'watch', describe: 'Watch a file', type: 'boolean' },
-        debug: {
-            describe: 'Show all skipped warnings and silent errors (beta)',
-            type: 'boolean'
-        },
-        project: {
-            describe: 'Compile all as project (alpha)',
-            type: 'boolean'
+    public static getInstance(): DyeScript {
+        if (!DyeScript.instance) {
+            DyeScript.instance = new DyeScript();
         }
-    }).argv;
+        return DyeScript.instance;
+    }
 
-const { path, output, target, watch, debug } = options;
-
-if (watch) {
-    watcher(path, output, target as DyeCompilerTarget, debug);
 }
 
-load(path, output, target as DyeCompilerTarget, debug);
+export default DyeScript;
