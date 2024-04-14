ca3bf920021f2db194a314f1ccfea14e709c9882
yokgs <yazid.slila01@gmail.com>
2024-04-14 20:41:03 +0100

    add : DyeBuilderTarget set

--- src/common/Target.ts
+++ src/common/Target.ts
@@ -8,4 +8,12 @@ const DyeTarget = {
 
 } as const;
 
+const DyeBuilderTarget = {
+    mincss: 'mincss',
+    dyeswift: 'dyeswift',
+    default: 'default'
+
+} as const;
+
 export type DyeCompilerTarget = keyof typeof DyeTarget;
+export type DyeBuilderTarget = keyof typeof DyeBuilderTarget;
