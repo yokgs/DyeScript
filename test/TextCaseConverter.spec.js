85aa3b3b9fc23058ca0b9adcf348560b60fd1f85
yokgs <yazid.slila01@gmail.com>
2024-04-14 20:14:45 +0100

    + textcase tests

---
+++ test/TextCaseConverter.spec.js
@@ -0,0 +1,22 @@
+const { TextCaseConverter } = require('../../dist/common/TextCaseConverter');
+const assert = require("assert");
+
+describe('TextCaseConverter', () => {
+  describe('toKebabCase', () => {
+    it('should convert a string to kebab case', () => {
+      assert.equal(TextCaseConverter.toKebabCase('WebkitTransform'), '-webkit-transform');
+      assert.equal(TextCaseConverter.toKebabCase('backgroundColor'), 'background-color');
+      assert.equal(TextCaseConverter.toKebabCase('helloWorld'), 'hello-world');
+      assert.equal(TextCaseConverter.toKebabCase('hello-world'), 'hello-world');
+    });
+  });
+
+  describe('toCamelCase', () => {
+    it('should convert a string to camel case', () => {
+      assert.equal(TextCaseConverter.toCamelCase('-webkit-transform'), 'WebkitTransform');
+      assert.equal(TextCaseConverter.toCamelCase('background-color'), 'backgroundColor');
+      assert.equal(TextCaseConverter.toCamelCase('hello-world'), 'helloWorld');
+      assert.equal(TextCaseConverter.toCamelCase('helloWorld'), 'helloWorld');
+    });
+  });
+});
