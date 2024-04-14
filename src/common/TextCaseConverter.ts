f8fc206726172ad6f7e5c00ba5bce122c922cdf9
yokgs <yazid.slila01@gmail.com>
2024-04-14 20:19:14 +0100

    fix : kebabcase Webkit, Moz, O => -webkit, -moz, -o

--- src/common/TextCaseConverter.ts
+++ src/common/TextCaseConverter.ts
@@ -1,7 +1,7 @@
 
 export class TextCaseConverter {
     public static toKebabCase(text: string): string {
-        return text.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+        return text.replace(/^([A-Z])/, '-$1').replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
     }
     public static toCamelCase(text: string): string {
         return text.replace(/-([a-z])/g, x => x[1].toUpperCase());
