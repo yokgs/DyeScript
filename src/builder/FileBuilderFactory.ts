88c70ec151475ece19b7e7ded1b75e040ff9d6a6
yokgs <yazid.slila01@gmail.com>
2024-04-14 20:42:30 +0100

    + internal store

--- src/builder/FileBuilderFactory.ts
+++ src/builder/FileBuilderFactory.ts
@@ -1,14 +1,29 @@
+import { DyeBuilderTarget } from "../common/Target";
+import { Store } from "../store/store";
 import { CSSBuilder } from "./CSSBuilder";
 import { DyeSwiftBuilder } from "./DyeSwiftBuilder";
 import { FileBuilder } from "./FileBuilder";
+import { MinCSSBuilder } from "./MinCSSBuilder";
 
 export class FileBuilderFactory {
-    static create(target: string): FileBuilder {
+    private store: Store;
+
+    constructor(store: Store) {
+        this.store = store;
+    }
+    static create(target: DyeBuilderTarget): FileBuilder {
         switch (target) {
-            case "dyeScript":
+            case "dyeswift":
                 return new DyeSwiftBuilder();
+            case "mincss":
+                return new MinCSSBuilder();
             default:
                 return new CSSBuilder();
         }
     }
+
+    public build(target: DyeBuilderTarget): string {
+        return FileBuilderFactory.create(target).build(this.store);
+    }
+
 }
