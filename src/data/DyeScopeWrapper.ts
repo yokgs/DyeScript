d59da527395a17642eeb1da9caee6b3f7077e051
yokgs <yazid.slila01@gmail.com>
2024-04-15 18:56:24 +0100

    add : variable setters

--- src/data/DyeScopeWrapper.ts
+++ src/data/DyeScopeWrapper.ts
@@ -1,11 +1,24 @@
 import { DyeScope } from "./DyeScope";
 
 export class DyeScopeWrapper {
+    
     private scope: DyeScope;
+
     constructor(scope: DyeScope) {
         this.scope = scope;
     }
+    
     public get(variable: string): any {
         return this.scope.get(variable);
     }
+    
+    public set(name: string, value: string): void {
+        return this.scope.set(name, value);
+    }
+    
+    public setDefault(name: string, value: string) {
+        if(this.scope.hasLocal(name)) return;
+        this.scope.set(name, value);
+    }
+
 }
