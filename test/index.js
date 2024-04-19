b9da5728e639849d0ecaf97013b285b39b1bfc66
yokgs <yazid.slila01@gmail.com>
2024-04-19 01:38:35 +0100

    + end to end testing

--- test/index.js
+++ test/index.js
@@ -1,13 +1,14 @@
 const DyeScript = require("../dist/index.js").default;
-const { CSSBuilder } = require("../dist/builder/CSSBuilder.js");
+const { MinCSSBuilder } = require("../dist/builder/MinCSSBuilder.js");
 const assert = require("assert");
 
 describe("DyeScript", function () {
   describe("#run()", function () {
     it("should return the correct store", function () {
-      // const dyeScript = new DyeScript();
-      // const store = dyeScript.run("$ body  , li, ul color red;^ body background-{^} blue {*} blue").getStore();
-      // console.log(new CSSBuilder().build(store));
+      const dyeScript = new DyeScript();
+      const store = dyeScript.run("$ body, li, ul color red;^ body background-{^} blue {*} blue").getStore();
+      const builder = new MinCSSBuilder();
+      assert.equal(builder.build(store), builder.header + 'body{color:blue;backgroundColor:blue}li{color:red}ul{color:red}');
     });
   });
 });
