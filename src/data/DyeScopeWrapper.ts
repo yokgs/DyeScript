d3b4391c12ad8cee90da8c95a0cb55d9aa591b7b
yokgs <yazid.slila01@gmail.com>
2024-04-20 01:20:48 +0100

    + class manipulations

--- src/data/DyeScopeWrapper.ts
+++ src/data/DyeScopeWrapper.ts
@@ -1,9 +1,11 @@
+import { WeightedValue } from "../common/style.interface";
+import { DyeRuntime } from "../core/DyeRuntime";
 import { DyeScope } from "./DyeScope";
 
 export class DyeScopeWrapper {
-
+    
     private scope: DyeScope;
-
+    
     constructor(scope: DyeScope) {
         this.scope = scope;
     }
@@ -23,6 +25,27 @@ export class DyeScopeWrapper {
 
     public update(scope: DyeScope) {
         this.scope = scope;
+    }
+
+    public applyClass(target: string, className: string) {
+        if (this.scope.classes.has(target)) {
+            const links = this.scope.links.get(target) || [];
+            links.push(className);
+            this.scope.links.set(target, links);
+        } else DyeRuntime.alert('undefined target');
+    }
+    
+    public extendClass(target: string, className: string) {
+        if (this.scope.classes.has(target)) {
+            const styles = this.scope.classes.get(target) || {};
+            const origin = this.scope.classes.get(className) || {};
+            for (let property in styles) {
+                let style = styles[property].map(value => [...value]) as WeightedValue[];
+                if (property in origin) origin[property] = style;
+                else origin[property].push(...style);
+            }
+            this.scope.classes.set(className, origin);
+        } else DyeRuntime.alert('undefined target');
     }
 
 }
