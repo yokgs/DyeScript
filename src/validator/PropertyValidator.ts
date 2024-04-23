1014c550e248b0007bf9209cdd72ff7f6fac80f2
yokgs <yazid.slila01@gmail.com>
2024-04-23 14:37:08 +0100

    add max length of css properties

--- src/validator/PropertyValidator.ts
+++ src/validator/PropertyValidator.ts
@@ -3,7 +3,7 @@ import { Validator } from "./Validator";
 export class PropertyValidator extends Validator {
     protected static test = {
         test(propertyName: string){
-            return /^(--|-)*[a-z]+[a-z-]*(?!-)$/.test(propertyName) && propertyName.length > 2 && propertyName.slice(-1) !== '-';
+            return propertyName.length > 2 && propertyName.slice(-1) !== '-' && propertyName.length < 64 && /^(--|-)*[a-z]+[a-z-]*(?!-)$/.test(propertyName);
         }
     };
     protected static strict = false;
