03963cf91ea9a5ad07405d77c4a6352c706c957e
yokgs <yazid.slila01@gmail.com>
2024-04-24 16:44:09 +0100

    fix : empty dyeswift file generation

--- src/builder/DyeSwiftBuilder.ts
+++ src/builder/DyeSwiftBuilder.ts
@@ -6,7 +6,7 @@ import { FileBuilder } from "./FileBuilder";
 
 export class DyeSwiftBuilder extends FileBuilder {
 
-    protected header: string = `!dyeswift;!version ${DyeRuntime.version};`;
+    protected header: string = `!dyeswift;!version ${DyeRuntime.version}\n`;
 
     constructor() {
         super();
@@ -29,14 +29,17 @@ export class DyeSwiftBuilder extends FileBuilder {
 
 
     processStyles(styles: Map<string, IStyle>) {
-        for (let el in styles) {
+        for (let el of styles.keys()) {
+            console.log("inside styles  ", el);
             this.append(`$ ${el}`);
             let style = styles.get(el);
             for (let p in style) {
                 this.append(` ${p} ${this.getDominantStyle(style[p])}`);
+                console.log(this.getOutput());
             }
             this.append(`\n`);
         }
+        console.log(this.getOutput());
     }
 
     processFonts(fonts: Map<string, IFont>) {
