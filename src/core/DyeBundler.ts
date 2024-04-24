f8b922cbef54594a29402817f8799f02ac8e1fee
yokgs <yazid.slila01@gmail.com>
2024-04-24 15:04:34 +0100

    add : bundler for multiple file generation

---
+++ src/core/DyeBundler.ts
@@ -0,0 +1,18 @@
+import { writeFileSync } from "fs";
+import { DyeBuilderTarget } from "../common/Target";
+
+export class DyeBundler {
+    static save(output: { data: string, target: DyeBuilderTarget}[], targetDirectory: string, name: string){
+        output.forEach(e=> {
+            writeFileSync(`${targetDirectory}/${name}.${this.toExt(e.target)}`, e.data);
+        })
+    }
+
+    static toExt(target: DyeBuilderTarget){
+        return {
+            "mincss": "min.css" as const,
+            "dyeswift": "s.dye" as const, 
+            "default": "css" as const
+        }[target];
+    }
+}
