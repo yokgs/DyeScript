5ad85630ef5977d6664cd1de1d86ee90ad721907
yokgs <yazid.slila01@gmail.com>
2024-04-26 12:14:30 +0100

    changing the default extension for dyegest file

--- src/core/DyeBundler.ts
+++ src/core/DyeBundler.ts
@@ -1,17 +1,18 @@
 import { writeFileSync } from "fs";
 import { DyeBuilderTarget } from "../common/Target";
+import path from "path";
 
 export class DyeBundler {
-    static save(output: { data: string, target: DyeBuilderTarget}[], targetDirectory: string, name: string){
-        output.forEach(e=> {
-            writeFileSync(`${targetDirectory}/${name}.${this.toExt(e.target)}`, e.data);
+    static save(output: { data: string, target: DyeBuilderTarget }[], targetDirectory: string, name: string) {
+        output.forEach(e => {
+            writeFileSync(path.join(targetDirectory, `${name}.${this.toExt(e.target)}`), e.data);
         })
     }
 
-    static toExt(target: DyeBuilderTarget){
+    static toExt(target: DyeBuilderTarget) {
         return {
             "mincss": "min.css" as const,
-            "dyegest": "s.dye" as const, 
+            "dyegest": "dyex" as const,
             "default": "css" as const
         }[target];
     }
