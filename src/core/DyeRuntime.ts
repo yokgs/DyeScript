4db6ae36d570e3daf9365f5037a22a143a8368ed
yokgs <yazid.slila01@gmail.com>
2024-04-26 12:15:35 +0100

    prevent dyegest to dyegest compilation

--- src/core/DyeRuntime.ts
+++ src/core/DyeRuntime.ts
@@ -12,63 +12,78 @@ export class DyeRuntime {
     private createdBy: string = 'api';
     private source: string;
     private static readonly globalScope = new DyeScope();
-
-    private static strictMode = false;
-
+    
+    private _strictMode = false;
+    private _isDigested = false;
+    
     constructor(source: string) {
         this.store = new Store();
         this.source = source;
         this.initilize();
     }
-
+    
     static createFromFile(filePath: string): DyeRuntime {
         const content = FileLoader.getInstance().load(filePath);
-        return new DyeRuntime(content);
+        let runtime = new DyeRuntime(content.content);
+        runtime.createdBy = content.path;
+        
+        if(content.ext === '.dyex') runtime.enableStrictMode();
+        
+        return runtime;
     }
-
+    
     static createFromContent(content: string): DyeRuntime {
         return new DyeRuntime(content);
     }
-
+    
     public restartFromSource(): this {
         this.store = null as unknown as Store;
         this.store = new Store();
         this.initilize();
         return this;
     }
-
+    
     private initilize() {
         let table = new Parser().parse(this.source);
-        let interpreter = new DyeInterpreter(this.store);
+        let interpreter = new DyeInterpreter(this.store, this);
         interpreter.process(table);
     }
-
+    
     public getStore(): Store {
         return this.store;
     }
-
+    
     public build(targets: DyeBuilderTarget[]) {
         let factory = new FileBuilderFactory(this.store);
-        return targets.map(target => {
+        const isNotDyeGest = !this.isDigested;
+        const nonRecursiveTargets = targets.filter(target => isNotDyeGest || (!isNotDyeGest && target != "dyegest"));
+        return nonRecursiveTargets.map(target => {
             return { data: factory.build(target), target };
         });
     }
-
+    
     static alert(message: string) {
-        if (DyeRuntime.strictMode) throw new Error(message);
-        else console.warn("[DyeRuntime] " + message);
+        console.warn("[DyeRuntime] " + message);
+    }
+    
+    public enableDyeGestMode() {
+        this._isDigested = true;
     }
 
     public enableStrictMode() {
-        DyeRuntime.strictMode = true;
+        this._strictMode = true;
     }
-
+    
     public disableStrictMode() {
-        DyeRuntime.strictMode = false;
+        this._strictMode = false;
+    }
+    
+    public get strictMode() {
+        return this._strictMode;
     }
 
-    public get strictMode() {
-        return DyeRuntime.strictMode;
+    public get isDigested() {
+        return this._isDigested;
     }
 
 }
