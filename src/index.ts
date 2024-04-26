236b79e91c59e762f91b8e4eaccd81451b849156
yokgs <yazid.slila01@gmail.com>
2024-04-26 12:10:21 +0100

    expose multi targets execution

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
+import path from "path";
+import { DyeBuilderTarget } from "./common/Target";
+import { DyeBundler } from "./core/DyeBundler";
 import { DyeRuntime } from "./core/DyeRuntime";
 
 class DyeScript {
-    private static instance: DyeScript;
-    private constructor() { }
 
-    public run(source: string): DyeRuntime {
-        return DyeRuntime.createFromContent(source);
+    public static run(content: string): DyeRuntime {
+        return DyeRuntime.createFromContent(content);
     }
 
-    public static getInstance(): DyeScript {
-        if (!DyeScript.instance) {
-            DyeScript.instance = new DyeScript();
-        }
-        return DyeScript.instance;
+    public static runFile(path: string): DyeRuntime {
+        return DyeRuntime.createFromFile(path);
     }
 
+    public static exec(source: string, output: string, targets: DyeBuilderTarget[]) {
+        const sourceName = path.parse(source).name;
+        const runtime = DyeScript.runFile(source);
+        const compiledSource =runtime.build(targets);
+        DyeBundler.save(compiledSource, output, sourceName);
+    }
 }
 
 export default DyeScript;
